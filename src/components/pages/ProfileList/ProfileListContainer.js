e17ecd1260269d4f61ce86e19a26821fe46efc31
Anthony Ware <aware91coding@gmail.com>
2021-02-01 21:31:03 -0600

    added nav bar to exampleList and profile page to test links

--- src/components/pages/ProfileList/ProfileListContainer.js
+++ src/components/pages/ProfileList/ProfileListContainer.js
@@ -6,21 +6,27 @@ import { getProfileData } from '../../../api';
 import { List } from '../../common';
 
 import RenderProfileListPage from './RenderProfileListPage';
+import NavBar from '../Nav/navBar';
+import '../Nav/navCSS.css';
 
 // Here is an example of using our reusable List component to display some list data to the UI.
 const ProfileList = () => {
   const { authState } = useOktaAuth();
 
   return (
-    <List
-      // Here we are passing our Axios request helper function as a callback.
-      getItemsData={() => getProfileData(authState)}
-      // Here we are passing in a component we want to show whilst waiting for our API request
-      // to complete.
-      LoadingComponent={() => <div>Loading Profiles...</div>}
-      // Here we are passing in a component that receives our new data and returns our JSX elements.
-      RenderItems={RenderProfileListPage}
-    />
+    <div>
+      <NavBar />
+      <p class="container"></p>
+      <List
+        // Here we are passing our Axios request helper function as a callback.
+        getItemsData={() => getProfileData(authState)}
+        // Here we are passing in a component we want to show whilst waiting for our API request
+        // to complete.
+        LoadingComponent={() => <div>Loading Profiles...</div>}
+        // Here we are passing in a component that receives our new data and returns our JSX elements.
+        RenderItems={RenderProfileListPage}
+      />
+    </div>
   );
 };
 
