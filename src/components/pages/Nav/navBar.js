06a5ecdee83dc40e202f2f560cbcfc9e9cfb6856
Anthony Ware <aware91coding@gmail.com>
2021-02-01 21:13:37 -0600

    Added :hover for user and favorites

--- src/components/pages/Nav/navBar.js
+++ src/components/pages/Nav/navBar.js
@@ -10,11 +10,17 @@ function NavBar(props) {
         <h1>CitySpire</h1>
       </Link>
       <div class="navLinks">
-        <Link to="/profile-list">
+        <Link to="/profile-list" class="users">
           <FaUserCircle size="30px" />
+          <div class="user">
+            <p>User</p>
+          </div>
         </Link>
-        <Link to="/example-list">
+        <Link to="/example-list" class="favorites">
           <FaHeart size="30px" />
+          <div class="heart">
+            <p>Favorites</p>
+          </div>
         </Link>
       </div>
     </div>
