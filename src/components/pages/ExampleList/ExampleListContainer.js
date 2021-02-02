156035bf1a49947c21c8703832da34ef5808f6a0
Anthony Ware <aware91coding@gmail.com>
2021-02-01 22:17:30 -0600

    set examples and profile back to original

--- src/components/pages/ExampleList/ExampleListContainer.js
+++ src/components/pages/ExampleList/ExampleListContainer.js
@@ -4,25 +4,19 @@ import { getExampleData } from '../../../api';
 
 import { List } from '../../common';
 import RenderExampleListPage from './RenderExampleListPage';
-import NavBar from '../Nav/navBar.js';
-import '../Nav/navCSS.css';
 
 // Here is an example of using our reusable List component to display some list data to the UI.
 const ExampleList = () => {
   return (
-    <div>
-      <NavBar />
-      <p class="container"></p>
-      <List
-        // Here we are passing our Axios request helper function as a callback.
-        getItemsData={getExampleData}
-        // Here we are passing in a component we want to show whilst waiting for our API request
-        // to complete.
-        LoadingComponent={() => <div>Loading Items...</div>}
-        // Here we are passing in a component that receives our new data and returns our JSX elements.
-        RenderItems={RenderExampleListPage}
-      />
-    </div>
+    <List
+      // Here we are passing our Axios request helper function as a callback.
+      getItemsData={getExampleData}
+      // Here we are passing in a component we want to show whilst waiting for our API request
+      // to complete.
+      LoadingComponent={() => <div>Loading Items...</div>}
+      // Here we are passing in a component that receives our new data and returns our JSX elements.
+      RenderItems={RenderExampleListPage}
+    />
   );
 };
 
--- src/components/pages/ProfileList/ProfileListContainer.js
+++ src/components/pages/ProfileList/ProfileListContainer.js
@@ -6,27 +6,21 @@ import { getProfileData } from '../../../api';
 import { List } from '../../common';
 
 import RenderProfileListPage from './RenderProfileListPage';
-import NavBar from '../Nav/navBar';
-import '../Nav/navCSS.css';
 
 // Here is an example of using our reusable List component to display some list data to the UI.
 const ProfileList = () => {
   const { authState } = useOktaAuth();
 
   return (
-    <div>
-      <NavBar />
-      <p class="container"></p>
-      <List
-        // Here we are passing our Axios request helper function as a callback.
-        getItemsData={() => getProfileData(authState)}
-        // Here we are passing in a component we want to show whilst waiting for our API request
-        // to complete.
-        LoadingComponent={() => <div>Loading Profiles...</div>}
-        // Here we are passing in a component that receives our new data and returns our JSX elements.
-        RenderItems={RenderProfileListPage}
-      />
-    </div>
+    <List
+      // Here we are passing our Axios request helper function as a callback.
+      getItemsData={() => getProfileData(authState)}
+      // Here we are passing in a component we want to show whilst waiting for our API request
+      // to complete.
+      LoadingComponent={() => <div>Loading Profiles...</div>}
+      // Here we are passing in a component that receives our new data and returns our JSX elements.
+      RenderItems={RenderProfileListPage}
+    />
   );
 };
 
