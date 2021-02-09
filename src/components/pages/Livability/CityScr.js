bb3eacb125ab25931e1ba403e936948a323f51a2
Anthony Ware <aware91coding@gmail.com>
2021-02-08 20:53:59 -0600

    backend heroku lacations db is set up endpoints are working

--- src/components/pages/Livability/CityScr.js
+++ src/components/pages/Livability/CityScr.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react';
 import './livabilityCSS.css';
 
 function CityScr() {
-  const [cityScr, setCityScr] = useState({});
+  const [cityScr, setCityScr] = useState([]);
 
   useEffect(() => {
     axios
-      .get('https://cityspire.dananderson.dev/city_scr/Seattle')
+      .get(`https://cityspire-d-be.herokuapp.com/locations`)
       .then(res => {
         setCityScr(res.data);
         console.log('aw: CityScr.js: axios: city_scr: ', res.data);
@@ -25,8 +25,30 @@ function CityScr() {
     <div className="mainScore">
       <h1>City Name</h1>
       <div className="subScores">
-        <h4>{cityScr.msg}</h4>
-        <p>{cityScr.score}</p>
+        {cityScr.map(cityScrs => (
+          <div key={cityScrs.id}>
+            <div>
+              <h4>Crime</h4>
+              <p>{cityScrs.crime_data}</p>
+            </div>
+            <div>
+              <h4>Population</h4>
+              <p>{cityScrs.population}</p>
+            </div>
+            <div>
+              <h4>Cost of Living</h4>
+              <p>{cityScrs.cost_of_living}</p>
+            </div>
+            <div>
+              <h4>Rental Rates</h4>
+              <p>{cityScrs.rental_rates}</p>
+            </div>
+            <div>
+              <h4>Walk Rate</h4>
+              <p>{cityScrs.walk_score}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/pages/Livability/LivabilityLandingPage.js
+++ src/components/pages/Livability/LivabilityLandingPage.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import AirQualScr from './AirQualScr.js';
 import CityScr from './CityScr.js';
-import CrimeScr from './CrimeScr.js';
 import './livabilityCSS.css';
-import RentAvg from './RentAvg.js';
-import WalkScr from './WalkScr.js';
 
 function LivabilityLandingPage() {
   return (
     <div className="livabilityBox">
       <CityScr />
-      <RentAvg />
-      <CrimeScr />
-      <WalkScr />
-      <AirQualScr />
     </div>
   );
 }
