5f8f08880b02e845de021b3b83a7390ac6f4747e
Anthony Ware <aware91coding@gmail.com>
2021-02-09 21:16:58 -0600

    added mapbox api json and able to pull up data

--- src/components/pages/Map/Map.js
+++ src/components/pages/Map/Map.js
@@ -1,14 +1,16 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import './Map.css';
 import LivabilityLandingPage from '../Livability/LivabilityLandingPage';
+import axios from 'axios';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function Map() {
   const mapContainerRef = useRef(null);
+  const [cityName, setCityName] = useState([]);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -31,7 +33,26 @@ function Map() {
     return () => map.remove();
   }, []);
 
-  const cityClicked = true;
+  useEffect(() => {
+    axios
+      // .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?access_token=pk.eyJ1IjoiY2l0eXNwaXJlZCIsImEiOiJja2tkcngzaHIwMHd3MnhsbWM0eHhibWtjIn0.FnQxPRtk2XQLf79gI3PFsw`)
+      .get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/seattle.json?access_token=pk.eyJ1IjoiY2l0eXNwaXJlZCIsImEiOiJja2tkcngzaHIwMHd3MnhsbWM0eHhibWtjIn0.FnQxPRtk2XQLf79gI3PFsw`
+      )
+      .then(res => {
+        setCityName(res.data);
+        console.log('aw: Map.js: setCityName: ', res.data.features); //.[0].geometry.coordinates)
+      })
+      .catch(err => {
+        console.log(
+          'aw: CityScr.js: axios: city_scr: ',
+          err.message,
+          err.response
+        );
+      });
+  }, []);
+
+  const cityClicked = false;
 
   return (
     <>
