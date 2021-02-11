bcef89adbe953fa7990c75df924fc85f8de763cf
Shane Jeremich <shane.jeremich@gmail.com>
2021-02-11 10:15:11 -0700

    deleted commented out text in LoginContainer testfile

--- src/__tests__/LoginContainer.test.js
+++ src/__tests__/LoginContainer.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { render } from '@testing-library/react';
 
 import LoginContainer from '../components/pages/Login/LoginContainer';
 
 describe('<LoginContainer /> test suite', () => {
   test('signin widget mounts successfully', () => {
-    let container = document.createElement('div');
+    const container = document.createElement('div');
     ReactDOM.render(<LoginContainer />, container);
     expect(container.querySelector('#sign-in-widget')).toBeTruthy();
   });
