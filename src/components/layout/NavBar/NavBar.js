42717dddf4fdc39bccdd49d8c5ccbde99da9b785
Shane Jeremich <shanejeremich@gmail.com>
2021-02-16 21:09:10 -0700

    cleaned up folder structure to follow container/render pattern

---
+++ src/components/layout/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHeart, FaUserCircle } from 'react-icons/fa';
+
+import './NavBar.css';
+
+function NavBar(props) {
+  return (
+    <div className="navBar">
+      <Link to="/">
+        <h1>CitySpire</h1>
+      </Link>
+      <div className="navLinks">
+        <Link to="/profile-list" className="users">
+          <FaUserCircle size="30px" />
+          <div className="user">
+            <p>User</p>
+          </div>
+        </Link>
+        <Link to="/example-list" className="favorites">
+          <FaHeart size="30px" />
+          <div className="heart">
+            <p>Favorites</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/pages/MapBox/MapboxContainer.js
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+import RenderMapbox from './RenderMapbox';
+import { LocationContext } from '../../../state/contexts';
+
+import './MapBox.css';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+function MapboxContainer() {
+  const mapContainerRef = useRef(null);
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99, 40],
+      zoom: 3.3,
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      countries: 'us',
+      mapboxgl: mapboxgl,
+    });
+
+    geocoder.on('result', function(result) {
+      setLocation(result.result.text);
+    });
+
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+    map.resize();
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <>
+      <LocationContext.Provider value={location}>
+        <RenderMapbox mapContainerRef={mapContainerRef} />
+      </LocationContext.Provider>
+    </>
+  );
+}
+export default MapboxContainer;
