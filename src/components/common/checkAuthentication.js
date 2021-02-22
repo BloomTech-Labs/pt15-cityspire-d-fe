236ed0bb3ee662704eb039e9a15ff1769865690a
Shane Jeremich <shane.jeremich@gmail.com>
2021-02-22 01:32:51 -0700

    made checkAuthentication a reusable function and put in common folder

---
+++ src/components/common/checkAuthentication.js
@@ -0,0 +1,14 @@
+export default async function checkAuthentication(
+  authState,
+  authService,
+  userInfo,
+  setUserInfo
+) {
+  const authenticated = await authState.isAuthenticated;
+  if (authenticated === authState.isAuthenticated) {
+    if (authenticated && !userInfo) {
+      const info = await authService.getUser();
+      setUserInfo(info);
+    }
+  }
+}
--- src/components/layout/Nav/NavBarContainer.js
+++ src/components/layout/Nav/NavBarContainer.js
@@ -4,21 +4,13 @@ import { useOktaAuth } from '@okta/okta-react';
 import RenderNavbar from './RenderNavbar';
 
 import './NavbarContainer.css';
+import { checkAuthentication } from '../../common';
 
 function Navbar() {
   const { authState, authService } = useOktaAuth();
   const [userInfo, setUserInfo] = useState(null);
 
-  async function checkAuthentication() {
-    const authenticated = await authState.isAuthenticated;
-    if (authenticated === authState.isAuthenticated) {
-      if (authenticated && !userInfo) {
-        const info = await authService.getUser();
-        setUserInfo(info);
-      }
-    }
-  }
-  checkAuthentication();
+  checkAuthentication(authState, authService, userInfo, setUserInfo);
 
   return (
     <div>
