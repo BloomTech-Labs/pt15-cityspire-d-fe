16ad1922879b651128038c88a5cd2c6e63a6ee97
Anthony Ware <aware91coding@gmail.com>
2021-02-21 20:36:19 -0600

    have a mouse enter and leave to show city data

--- src/components/pages/Favorite/RenderFavoritesPage.js
+++ src/components/pages/Favorite/RenderFavoritesPage.js
@@ -6,27 +6,12 @@ function RenderFavoritesPage() {
   const [favorite, setFavorite] = useState([]);
   const [userId, setUserId] = useState([]);
   const [cityScr, setCityScr] = useState([]);
+  const [isShown, setIsShown] = useState(false);
 
-  console.log('aw: find user: ');
-
-  // useEffect(() => {
-  //     axios
-  //     .get(`https://cityspire-d-be.herokuapp.com/userlocations`)
-  //     .then(res => {
-  //         setUserId(res.data);
-  //         console.log('aw: RenderFavoritesPage.js: .get: userID', res.data);
-  //     })
-  //     .catch(err => {
-  //         console.log(
-  //         'aw: RenderFavoritesPage.js: .get: userID: ',
-  //         err.message,
-  //         err.response
-  //         );
-  //     });
-  // }, []);
+  // console.log('aw: find user: ');
 
   useEffect(() => {
-    axios
+    axios // get users favorite locations
       // .get(`https://cityspire-d-be.herokuapp.com/userlocations/${userId}`)
       // llama002
       // .get(`https://cityspire-d-be.herokuapp.com/userlocations/00ultwew80Onb2vOT4x6`)
@@ -48,7 +33,7 @@ function RenderFavoritesPage() {
           err.response
         );
       });
-    axios
+    axios // get location data
       .get(`https://cityspire-d-be.herokuapp.com/locations`)
       .then(res => {
         setCityScr(res.data);
@@ -70,7 +55,41 @@ function RenderFavoritesPage() {
       <p>click go back to map</p>
       {favorite.map(userLoc => (
         <div key={userLoc.id}>
-          <h2 className="cities">{userLoc.name}</h2>
+          <button
+            className="cities"
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+          >
+            {userLoc.name}
+          </button>
+          {isShown && (
+            <div className="data">
+              {cityScr.map(cityScrs => (
+                <div key={cityScrs.id}>
+                  <div>
+                    <h4>Crime</h4>
+                    <p>{cityScrs.crime_data}</p>
+                  </div>
+                  <div>
+                    <h4>Population</h4>
+                    <p>{cityScrs.population}</p>
+                  </div>
+                  <div>
+                    <h4>Cost of Living</h4>
+                    <p>{cityScrs.cost_of_living}</p>
+                  </div>
+                  <div>
+                    <h4>Rental Rates</h4>
+                    <p>{cityScrs.rental_rates}</p>
+                  </div>
+                  <div>
+                    <h4>Walk Rate</h4>
+                    <p>{cityScrs.walk_score}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       ))}
     </div>
