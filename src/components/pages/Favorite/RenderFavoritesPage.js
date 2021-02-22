226fecd3baa04b9eed187a244eabe6fe4a2da407
Anthony Ware <aware91coding@gmail.com>
2021-02-21 20:19:14 -0600

    I have the cities show up with heard coded user Id

--- src/components/pages/Favorite/RenderFavoritesPage.js
+++ src/components/pages/Favorite/RenderFavoritesPage.js
@@ -1,29 +1,39 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import './Favorites.css';
 
 function RenderFavoritesPage() {
   const [favorite, setFavorite] = useState([]);
   const [userId, setUserId] = useState([]);
+  const [cityScr, setCityScr] = useState([]);
+
+  console.log('aw: find user: ');
+
+  // useEffect(() => {
+  //     axios
+  //     .get(`https://cityspire-d-be.herokuapp.com/userlocations`)
+  //     .then(res => {
+  //         setUserId(res.data);
+  //         console.log('aw: RenderFavoritesPage.js: .get: userID', res.data);
+  //     })
+  //     .catch(err => {
+  //         console.log(
+  //         'aw: RenderFavoritesPage.js: .get: userID: ',
+  //         err.message,
+  //         err.response
+  //         );
+  //     });
+  // }, []);
 
   useEffect(() => {
     axios
-      .get(`https://cityspire-d-be.herokuapp.com/userlocations`)
-      .then(res => {
-        setUserId(res.data);
-        console.log('aw: RenderFavoritesPage.js: .get: userID', res.data);
-      })
-      .catch(err => {
-        console.log(
-          'aw: RenderFavoritesPage.js: .get: userID: ',
-          err.message,
-          err.response
-        );
-      });
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get(`https://cityspire-d-be.herokuapp.com/userlocations/${userId}`)
+      // .get(`https://cityspire-d-be.herokuapp.com/userlocations/${userId}`)
+      // llama002
+      // .get(`https://cityspire-d-be.herokuapp.com/userlocations/00ultwew80Onb2vOT4x6`)
+      // llama001
+      .get(
+        `https://cityspire-d-be.herokuapp.com/userlocations/00ulthapbErVUwVJy4x6`
+      )
       .then(res => {
         setFavorite(res.data);
         console.log(
@@ -38,13 +48,29 @@ function RenderFavoritesPage() {
           err.response
         );
       });
+    axios
+      .get(`https://cityspire-d-be.herokuapp.com/locations`)
+      .then(res => {
+        setCityScr(res.data);
+        console.log('aw: CityScr.js: .get: setCityScr: ', res.data);
+      })
+      .catch(err => {
+        console.log(
+          'aw: CityScr.js: .get: city_scr: ',
+          err.message,
+          err.response
+        );
+        setCityScr(null);
+      });
   }, []);
 
   return (
-    <div>
+    <div className="favorites">
+      <p>hover see city data</p>
+      <p>click go back to map</p>
       {favorite.map(userLoc => (
         <div key={userLoc.id}>
-          <h2>{userLoc.name}</h2>
+          <h2 className="cities">{userLoc.name}</h2>
         </div>
       ))}
     </div>
